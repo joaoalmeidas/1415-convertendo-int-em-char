be0a997c77c453928ee81d9d256a752d8c6cac81
joao <joao182cx@gmail.com>
2019-02-19 14:27:27 -0300

    Criação do array de caracteres.

---
+++ src/IntCaracteres.java
@@ -0,0 +1,16 @@
+
+public class IntCaracteres {
+
+	public static void main(String[] args) {
+		
+		char[] caracteres = new char[255];
+		
+		for(int i = 0; i <= caracteres.length; i++) {
+			
+			caracteres[i] = (char) i;
+			
+		}
+		
+	}
+
+}
