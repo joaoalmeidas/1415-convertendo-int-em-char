f6088221f02363d4c645c0dee804af1d1f72c10a
joao <joao182cx@gmail.com>
2019-02-19 14:37:26 -0300

    Criação do laço de repetição que exibe os caracteres de 0 a 255na tela.

--- src/IntCaracteres.java
+++ src/IntCaracteres.java
@@ -3,11 +3,23 @@ public class IntCaracteres {
 
 	public static void main(String[] args) {
 		
-		char[] caracteres = new char[255];
+		char[] caracteres = new char[256];
 		
-		for(int i = 0; i <= caracteres.length; i++) {
+		for(int i = 0; i < caracteres.length; i++) {
 			
 			caracteres[i] = (char) i;
+			
+		}
+		
+		for(int i = 0; i < caracteres.length; i++) {
+			
+			System.out.printf("%c ", caracteres[i]);
+			
+			if(i%20 == 0) {
+				
+				System.out.println();
+				
+			}
 			
 		}
 		
